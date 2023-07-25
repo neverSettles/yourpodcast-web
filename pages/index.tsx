104815ac4b5607896e7eafcc0814c63459e68839
neverSettles <christopherRSettles@gmail.com>
2023-07-25 08:33:31 -0700

    Change from blue background to white

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ const Home: NextPage = () => {
   const { data, status } = useSession();
   if (status === 'authenticated') {
     return (
-      <div className="bg-gradient-to-r from-blue-700 via-blue-800 to-blue-900 min-h-screen flex flex-col items-center justify-center py-2">
+      <div className="bg-gradient-to-r from-white-700 via-white-800 to-white-900 min-h-screen flex flex-col items-center justify-center py-2">
         <Head>
           <title>AI Podcast Generator</title>
           <link rel="icon" href="/favicon.ico" />
@@ -91,14 +91,14 @@ const Home: NextPage = () => {
             <div className="flex items-center space-x-3 mt-4">
               <p className="text-left font-medium">3. Duration (min)</p>
             </div>
-            <input
+            {/* <input
               value={duration}
               type='number'
               max={15}
               onChange={(e) => setDuration(Number(e.target.value))}
               className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring focus:ring-black mt-2 p-3"
               placeholder="10"
-            />
+            /> */}
             {!loading ? (
               <button
                 className="bg-black rounded-lg text-white font-medium px-4 py-2 mt-8 w-full hover:bg-black/80"
@@ -146,7 +146,7 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="bg-gradient-to-r from-blue-700 via-blue-800 to-blue-900 min-h-screen flex flex-col items-center justify-center py-2">
+    <div className="bg-gradient-to-r from-white-700 via-white-800 to-white-900 min-h-screen flex flex-col items-center justify-center py-2">
       <Head>
         <title>AI Podcast Generator</title>
         <link rel="icon" href="/favicon.ico" />
@@ -154,7 +154,7 @@ const Home: NextPage = () => {
 
       <Header />
       <main className="flex flex-1 flex-col items-center justify-center text-center mt-12 sm:mt-20">
-        <h1 className="text-5xl font-bold text-white mt-6">
+        <h1 className="text-5xl font-bold text-black mt-6">
           Welcome to AI Podcast Generator
         </h1>
         <button onClick={() => signIn('google')} className="bg-black rounded-lg text-white font-medium px-4 py-2 mt-8 w-full hover:bg-black/80">
