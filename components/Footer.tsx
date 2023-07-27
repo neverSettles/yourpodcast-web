b1abf0fba23a2aec79b74f2f5d1140c195831d87
neverSettles <christopherRSettles@gmail.com>
2023-07-26 22:36:04 -0700

    Remove vercel edge functions, add AWS Polly

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
-        Powered by{" "}
+      Powered by{" "}
         <a
           href="https://openai.com/blog/chatgpt"
           target="_blank"
@@ -15,12 +15,12 @@ export default function Footer() {
         </a>
         and{" "}
         <a
-          href="https://vercel.com/"
+          href="https://aws.amazon.com/polly/"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
         >
-          Vercel Edge Functions.
+          AWS Polly.
         </a>
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
