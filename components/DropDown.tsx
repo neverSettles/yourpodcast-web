86e48ed97dba5556e0a6ab604241b7824fe55dce
neverSettles <christopherRSettles@gmail.com>
2023-07-30 05:34:57 -0700

    Add drop down

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,14 +10,14 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Professional" | "Pretentious" | "Story";
+export type VibeType = "Professional" | "Pretentious" | "Story" | "Emotionally charged";
 
 interface DropDownProps {
   vibe: VibeType;
   setVibe: (vibe: VibeType) => void;
 }
 
-let vibes: VibeType[] = ["Professional","Pretentious","Story"];
+let vibes: VibeType[] = ["Professional","Pretentious","Story", "Emotionally charged"];
 
 export default function DropDown({ vibe, setVibe }: DropDownProps) {
   return (
