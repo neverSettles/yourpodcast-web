20e0639c40637f122d60346f530ac2cde29bc6c9
IsaacWatt <isaacwatt@icloud.com>
2023-11-25 21:36:45 -0800

    new link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://github.com/neverSettles/yourpodcast-web"
+          href="https://github.com/IsaacWatt/Podcast-BE"
           className="group"
           aria-label="Podcast"
         >
