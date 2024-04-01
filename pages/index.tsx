06ff09bbb5215cad65430f4be37ea8d28d458685
Chris Settles <ChristopherRSettles@gmail.com>
2024-03-31 19:15:01 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -224,7 +224,7 @@ const Home: NextPage = () => {
                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
                   loading="lazy">
               </iframe> */}
-            <iframe style={{ borderRadius: '12px' }} src="https://open.spotify.com/embed/show/6LiNaGTDMiUJqxh9e8p5Z5?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe style={{ borderRadius: '12px' }} src="https://open.spotify.com/embed/show/6LiNaGTDMiUJqxh9e8p5Z5?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
           </div>
         </main>
         <Footer />
